8435327052268ce61a139e7bc812d6955983e7a6
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-07-20 15:11:12 -0400

    directive app-mapa adicionada em postos component

---
+++ src/app/mapa/mapa.module.ts
@@ -0,0 +1,14 @@
+import { MapaComponent } from './mapa.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MapaComponent
+  ],
+  declarations: []
+})
+export class MapaModule { }
---
+++ src/app/postos/postos.module.ts
@@ -0,0 +1,12 @@
+import { MapaModule } from './../mapa/mapa.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MapaModule
+  ],
+  declarations: []
+})
+export class PostosModule { }
