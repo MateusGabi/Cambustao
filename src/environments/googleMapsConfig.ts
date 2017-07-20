e49ec3d28559b4d27d314b921de81e8d6dd2d70e
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-07-20 11:30:44 -0400

    adicionado config do google maps

---
+++ src/environments/googleMapsConfig.ts
@@ -0,0 +1,6 @@
+
+export const googleMapsConfig = {
+    apiKey : "AIzaSyDfg2KLaFH9ZaOIvNlcCWWdx2dmj6imhR4",
+    serverKey : "AIzaSyCgV3S1aMNv0cpsEOo8KtVk33hfcRPyURM",
+    browserKey: "AIzaSyBTz-KXEqQ0YZ6_u4PDKa2bzhivSjwTbsw"
+};
