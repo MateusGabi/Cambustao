89861053dc87b8912e516ed895e637ddd207022e
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-07-20 09:18:41 -0400

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyBTz-KXEqQ0YZ6_u4PDKa2bzhivSjwTbsw",
+    authDomain: "cambustao.firebaseapp.com",
+    databaseURL: "https://cambustao.firebaseio.com",
+    storageBucket: "cambustao.appspot.com",
+    messagingSenderId: "183512928699"
+};
+
