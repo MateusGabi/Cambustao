248e7cca73700b557934798c78a1e7eb48b7d6ac
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-07-22 11:45:47 -0400

    adicionado acoes quando clica na acao rapida

--- src/app/acoes-rapidas-dashboard/acoes-rapidas-dashboard.component.ts
+++ src/app/acoes-rapidas-dashboard/acoes-rapidas-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcoesRapidasDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onClick(event : EventListener, value : string) {
+    console.log(value);
+
+    switch (value) {
+      case 'caminhoes':
+        this.router.navigate(['/caminhoes']);
+        break;
+      case 'motoristas':
+        this.router.navigate(['/motoristas']);
+        break;
+      case 'postos':
+        this.router.navigate(['/postos']);
+        break;
+      case 'viagens':
+        this.router.navigate(['/viagens']);
+        break;
+    
+      default:
+        break;
+    }
   }
 
 }
