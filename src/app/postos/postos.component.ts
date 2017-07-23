178c8326939ebd1fbaf54a483b133e25b6956b70
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-07-23 10:58:10 -0400

    Adicionado InfoWindow quando cicado em um marcador de posto

--- src/app/postos/postos.component.ts
+++ src/app/postos/postos.component.ts
@@ -57,16 +57,30 @@ export class PostosComponent implements OnInit {
 		var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 4,
 			center: {lat: -16.4483014, lng: -68.9872348}
-		});
+    });
 
 		(this.db.list("/postos", {preserveSnapshot:true})).subscribe(snapshots => {
 			snapshots.forEach(posto => {
-				new google.maps.Marker({
+				var marker = new google.maps.Marker({
 					position: posto.val().location,
-					map: map
-				}).addListener('click', function() {
+          map: map,
+          title: posto.val().nome
+        });
+
+        console.log(posto.val());
+
+        var contentString = '<h4>'+ (posto.val().nome || 'Sem nome :/') +'</h4>' + 
+                            '<h5><b>Endereço:</b> '+ posto.val().endereco +'</h5>' + 
+                            '<h5><b>Preço diesel:</b> R$'+ posto.val().preco_diesel +'</h5>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        
+        marker.addListener('click', function() {
 					map.setZoom(14);
-					map.setCenter(posto.val().location);
+          map.setCenter(posto.val().location);
+          infowindow.open(map, marker);
 				});
 			});
 		});
