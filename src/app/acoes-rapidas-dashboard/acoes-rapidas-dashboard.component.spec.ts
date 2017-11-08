fe0b69342c739d72c8c106c7c80099a285726787
Pedro Moraes <pedro_hmoraes@hotmail.com>
2017-11-08 17:53:31 -0200

    Acoes Rapidas Test Funfando

--- src/app/acoes-rapidas-dashboard/acoes-rapidas-dashboard.component.spec.ts
+++ src/app/acoes-rapidas-dashboard/acoes-rapidas-dashboard.component.spec.ts
@@ -1,14 +1,33 @@
+import { HomePageComponent } from './../pages/home-page.component';
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AcoesRapidasDashboardComponent } from './acoes-rapidas-dashboard.component';
+import { APP_BASE_HREF } from '@angular/common';
 
+const routeStub : Routes = [
+  {
+    path: '', component : AcoesRapidasDashboardComponent
+  }
+];
 describe('AcoesRapidasDashboardComponent', () => {
   let component: AcoesRapidasDashboardComponent;
   let fixture: ComponentFixture<AcoesRapidasDashboardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AcoesRapidasDashboardComponent ]
+      declarations: [ AcoesRapidasDashboardComponent ],
+      providers: [
+        {
+          provide : APP_BASE_HREF, useValue : '/'
+        }
+      ],
+      imports: [
+        RouterModule.forRoot(routeStub),
+      ]
     })
     .compileComponents();
   }));
